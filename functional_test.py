8ca774d2e27fcc97121e9e8b920ee08c92f4b21a
eigenvector7 <nish_c@live.com>
2017-12-27 19:46:35 -0800

    Updating the functional test file with credit to Obey the goat book

--- functional_test.py
+++ functional_test.py
@@ -1,3 +1,6 @@
+#Functional Test for Django powered rest APIs
+#credit book titled: "Test driven development with Python"
+#credit book author: Percival
 from selenium import webdriver
 browser = webdriver.Chrome()
 browser.get('http://localhost:8000')
